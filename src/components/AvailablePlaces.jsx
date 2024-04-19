3c5d46f6d5f3bca62a662812bc9a618938e64e06
Amisha PRIYA <priyaamisha03@gmail.com>
2024-04-20 01:29:35 +0530

    Con of using fetch to get the data from backend API

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // HTTP request to fetch data from the backend API
+
+  // 1st option -> will cause an infinite loop, so not preferred
+  // fetch method is provided by the browser
+
+  // fetch('https://localhost:3000/places').then((response) => {
+  //   return response.json();
+  // }).then((data) => setAvailablePlaces(data.places));
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
