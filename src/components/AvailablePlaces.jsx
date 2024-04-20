5ae947efbcb2aec7f8baa6784f4c764e03617313
Amisha PRIYA <priyaamisha03@gmail.com>
2024-04-20 14:04:18 +0530

    Use async await on fetch method

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -13,11 +13,23 @@ export default function AvailablePlaces({ onSelectPlace }) {
   //   return response.json();
   // }).then((data) => setAvailablePlaces(data.places));
 
+  // 2nd option ->
   // useEffect to solve the infinite loop issue. fetch method will be executed only once when the component is loaded for the first
+  // useEffect(() => {
+  //     fetch('http://localhost:3000/places').then((response) => {
+  //       return response.json();
+  //     }).then((data) => setAvailablePlaces(data.places));
+  // }, []);
+
+  // 3rd option, most modern ->
+  // We cannot use async directly on react specific elements like component functions, callback functions used in react hooks etc
   useEffect(() => {
-      fetch('http://localhost:3000/places').then((response) => {
-        return response.json();
-      }).then((data) => setAvailablePlaces(data.places));
+    async function fetchData() {
+      const response =  await fetch('http://localhost:3000/places');
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+    }
+    fetchData();
   }, []);
 
   return (
